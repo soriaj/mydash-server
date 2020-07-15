f2e75d5132041915fcc92ac24ab58558619fef2a
soriaj <javier.soria@gmail.com>
2020-07-15 09:49:24 -0700

    - Create SQL migration templates - Add migration script to package.json scripts - Add database connections to config - Add database connections to server configuration - Create postgrator configuration

--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
+require('dontenv').config()
+
+const knew = require('knew')
 const app = require('./app')
-const { PORT } = require('./config')
+const { PORT, DATABASE_URL } = require('./config')
+
+const db = knew({
+   client: 'pg',
+   connection: TEST_DATABASE_URL,
+})
+
+app.set('db', db)
 
 app.listen(PORT, () => {
    console.log(`Server listening at http://localhost:${PORT}`)
